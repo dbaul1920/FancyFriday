da59ced85757068a61b7477f3b9f45f8a1cb2e78
Domonique Baul <dbaul1920@gmail.com>
2016-09-08 11:43:04 -0400

    collecting user imput

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 /**
  * 
  */
-
+/* This is a demo app for picking out outfits */
 /**
  * @author DomB
  *
@@ -13,6 +15,27 @@ public class MainApp {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			String inputFancyFriday, inputSunny, inputRaining;
+			boolean fancyFriday, suny, raining;
+		
+		// Get input from user
+			Scanner input = new Scanner(System.in);
+			System.out.println("Is it Fancy Friday?");
+			inputFancyFriday = input.nextLine();
+			
+			System.out.println("Is it sunny?");
+			inputSunny = input.nextLine();
+			
+			System.out.println("Is it raining?");
+			inputRaining = input.nextLine();
+		
+		// Quick test goes here	
+			System.out.println(inputFancyFriday + " " + inputSunny + " " + inputRaining);
+			
+		// Set condition variables
+		
+		// Use if  statements to decide what to wear
+		
 
 	}
 
