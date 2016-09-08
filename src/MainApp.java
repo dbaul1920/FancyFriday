3c85e760dfc000529a98cff362218cf1cc807476
Domonique Baul <dbaul1920@gmail.com>
2016-09-08 12:02:37 -0400

    add Variables

--- src/MainApp.java
+++ src/MainApp.java
@@ -16,7 +16,9 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			String inputFancyFriday, inputSunny, inputRaining;
-			boolean fancyFriday, suny, raining;
+			boolean fancyFriday = false;
+			boolean sunny = false;
+			boolean raining = false;
 		
 		// Get input from user
 			Scanner input = new Scanner(System.in);
@@ -29,10 +31,23 @@ public class MainApp {
 			System.out.println("Is it raining?");
 			inputRaining = input.nextLine();
 		
-		// Quick test goes here	
-			System.out.println(inputFancyFriday + " " + inputSunny + " " + inputRaining);
+		// Quick test goes here	- Just making sure it works 
+		//	System.out.println(inputFancyFriday + " " + inputSunny + " " + inputRaining);
 			
 		// Set condition variables
+			
+			if (inputFancyFriday.equalsIgnoreCase("y")){
+				fancyFriday = true;				
+			}
+			if (inputSunny.equalsIgnoreCase("y")){
+				sunny = true;				
+			}
+			if (inputRaining.equalsIgnoreCase("y")){
+				raining = true;				
+			}
+			
+		//Boolean test	
+			System.out.println(fancyFriday + " " + sunny + " " + raining);
 		
 		// Use if  statements to decide what to wear
 		
